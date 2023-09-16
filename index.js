1f5cce8f6f037108956b68b6ca2ea83520ff9cec
Joseph Chan <josephchan36@gmail.com>
2023-09-15 23:58:23 -0700

    first push

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const express = require("express")
+const app = express();
+
+app.get("/",(req, res) =>{
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+app.listen(process.env.PORT || 5500) 
 class Note{
     constructor(id, text){
         this.id = id
